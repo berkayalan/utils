7913462f72554b0a59a704078a1828a67e78e8c7
Berkay Alan <berkayalan.mail@gmail.com>
2022-04-18 13:58:40 +0300

    last hour added

--- date_utils.py
+++ date_utils.py
@@ -10,3 +10,6 @@ df["date"] =  df["rtimestamp"].apply(lambda x: x.date())
 df["year"] =  df["rtimestamp"].apply(lambda x: x.year)
 df["month"] =  df["rtimestamp"].apply(lambda x: x.month)
 df["day"] =  df["rtimestamp"].apply(lambda x: x.day)
+
+# getting last hour
+pd.to_datetime(datetime.now() - timedelta(hours = 1)).strftime('%Y/%m/%d/%H')
